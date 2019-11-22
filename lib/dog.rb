067772032c8455ebb5781caff0432268f432f253
alexiscait142 <chilinski.a@gmail.com>
2019-11-22 13:48:49 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    attr_reader :name
+
+    # def initialize name
+    #     @name = name
+    # end
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+    attr_reader :name
+
+    # def initialize name
+    #     @name = name
+    # end
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+end
